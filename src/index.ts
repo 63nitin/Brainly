1f9d7d411d15c96c847da2b3c362280c42fd2f81
63nitin <yadavnitin638675@bbdnitm.ac.in>
2025-08-24 13:30:34 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+const app = express();
+
+
+app.get("/api/v1/singup", (req, res) => {
+
+});
+
+app.post('/api/v1/singin', (req, res) => {
+
+})
